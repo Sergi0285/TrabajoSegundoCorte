268b15ca5b4af4e5bc6ada6fc95378d09aa22caf
MeliV1 <laura.vargas03@usa.edu.co>
2024-09-30 21:53:16 -0500

    Carpetas con springboot

---
+++ src/main/java/Trabajo/Ingenieria/IngenieriaApplication.java
@@ -0,0 +1,13 @@
+package Trabajo.Ingenieria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IngenieriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IngenieriaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Trabajo/Ingenieria/IngenieriaApplicationTests.java
@@ -0,0 +1,13 @@
+package Trabajo.Ingenieria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IngenieriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
